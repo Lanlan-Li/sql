ef358ca0c9a58d55b6799516e38356e11e934e98
Lanlan Li <59808983+Lanlan-Li@users.noreply.github.com>
2025-11-22 16:46:07 -0500

    Add files via upload

--- 02_activities/assignments/Cohort_8/assignment1.sql
+++ 02_activities/assignments/Cohort_8/assignment1.sql
@@ -4,17 +4,25 @@
 
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
-
+SELECT*FROM customer;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-
+SELECT*FROM customer
+order by customer_last_name, customer_first_name
+LIMIT 10; 
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
+SELECT * FROM customer_purchases
+WHERE product_id IN (4,9);
 
+SELECT * 
+FROM customer_purchases
+WHERE product_id = 4
+OR product_id = 9;
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
@@ -26,7 +34,11 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 
 
 -- option 2
+SELECT*, 
+quantity*cost_to_customer_per_qty as [price]
 
+FROM customer_purchases
+WHERE customer_id BETWEEN '8' AND '10';
 
 
 --CASE
@@ -35,20 +47,35 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
+SELECT product_id, product_name,
+
+CASE WHEN product_qty_type = 'unit' THEN 'unit'
+	ELSE 'bulk'
+	END as prod_qty_type_condensed
 
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
+,CASE WHEN product_name LIKE '%pepper%'
+THEN '1'
+ELSE '0'
+END AS pepper_flag
 
+FROM product;
 
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+SELECT vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name,
+market_date, booth_number,
+v.vendor_id
 
-
-
+FROM vendor as v
+INNER JOIN vendor_booth_assignments as vbs
+	ON v.vendor_id = vbs.vendor_id
+ORDER BY vendor_name, market_date;
 
 /* SECTION 3 */
 
@@ -56,6 +83,11 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
+SELECT vendor_id
+, count ( vendor_id) as booth_rent_times
+
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
@@ -64,7 +96,16 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 
+SELECT customer_last_name, customer_first_name, c.customer_id
+ ,sum(quantity*cost_to_customer_per_qty) as total_expense
+ 
+ FROM customer as c
+  inner join customer_purchases as cp
+	on c.customer_id = cp.customer_id
 
+GROUP by cp.customer_id
+HAVING total_expense > 2000
+ORDER by customer_last_name, customer_first_name;
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -77,6 +118,17 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+
+DROP TABLE IF EXISTS temp.new_vendor;
+
+CREATE TABLE temp.new_vendor as
+SELECT*from vendor;
+
+INSERT INTO new_vendor
+
+VALUES(10, 'Thomas Superfood Store', 'Fresh Focused store', 'Rosenthal', 'Thomas');
+
+SELECT*from new_vendor;
 
 
 
